b71c2506320e960756aa12a412fa688c52c67b8e
Andrew Fedorchenko <furturnax@gmail.com>
2023-12-28 16:57:38 +0300

    All fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
